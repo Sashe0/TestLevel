10cf25644b1d7d504a5b4d9f183af6387338506a
Sashe0 <sashabodnar11@gmail.com>
2025-09-24 16:19:17 +0300

    Initial Commit

---
+++ TestLevel/TestLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLevelApp.swift
+//  TestLevel
+//
+//  Created by Саша Боднар on 24.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
